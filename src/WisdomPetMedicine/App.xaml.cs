2d8d26f5da025174898d3e9f0c483fa37e44e064
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-24 11:31:35 +0200

    Initial commit

---
+++ src/WisdomPetMedicine/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WisdomPetMedicine;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/WisdomPetMedicine/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WisdomPetMedicine;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
